2355eb453cdd47f7d8a9bb632f6a3299ae4c1dde
Ian Hess <ian.h.hess@gmail.com>
2023-01-05 14:24:46 -0500

    comment out old way

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-if (window.XMLHttpRequest) {
-  request = new XMLHttpRequest();
-} else if (window.ActiveXObject) {
-  try {
-    request = new ActiveXObject("Msxml2.XMLHTTP");
-  } catch (e) {
-    try {
-      request = new ActiveXObject("Microsoft.XMLHTTP");
-    } catch (e) {}
-  }
-}
+//Old way
+//  if (window.XMLHttpRequest) {
+//   request = new XMLHttpRequest();
+// } else if (window.ActiveXObject) {
+//   try {
+//     request = new ActiveXObject("Msxml2.XMLHTTP");
+//   } catch (e) {
+//     try {
+//       request = new ActiveXObject("Microsoft.XMLHTTP");
+//     } catch (e) {}
+//   }
+// }
 
-request.open("GET", "https://url.com/placeholder", true);
-request.send(null);
+// request.open("GET", "https://url.com/placeholder", true);
+// request.send(null);
