3f649a431014804f802476a51128a9dcb355546e
Ian Hess <ian.h.hess@gmail.com>
2023-01-05 14:23:26 -0500

    type tutorial boilerplate

---
+++ script.js
@@ -0,0 +1,14 @@
+if (window.XMLHttpRequest) {
+  request = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+  try {
+    request = new ActiveXObject("Msxml2.XMLHTTP");
+  } catch (e) {
+    try {
+      request = new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (e) {}
+  }
+}
+
+request.open("GET", "https://url.com/placeholder", true);
+request.send(null);
