9b2199eb015870c253cbea994b1dccd6e660491e
Ian Hess <ian.h.hess@gmail.com>
2023-01-05 15:52:21 -0500

    add html file

--- script.js
+++ script.js
@@ -13,3 +13,15 @@
 
 // request.open("GET", "https://url.com/placeholder", true);
 // request.send(null);
+
+//URL (required), options (optional)
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=YvoQbpCUoJR805aNj9d7PZJNM7lw6sTz&s=cats",
+  { mode: "cors" }
+)
+  .then(function (response) {
+    //successful response
+  })
+  .catch(function (err) {
+    //error
+  });
